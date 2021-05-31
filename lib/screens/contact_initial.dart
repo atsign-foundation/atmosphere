bc78b794a9fead1580835ced520bd5ea02f95541
Sachin Singh <sachins@geekyants.com>
2021-05-31 18:08:15 +0530

    contact initial widget added

---
+++ lib/screens/contact_initial.dart
@@ -0,0 +1,36 @@
+import 'package:atsign_atmosphere_app/utils/colors.dart';
+import 'package:atsign_atmosphere_app/utils/text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:atsign_atmosphere_app/services/size_config.dart';
+
+class ContactInitial extends StatelessWidget {
+  final double size;
+  final String initials;
+  int index;
+
+  ContactInitial({Key key, this.size = 40, @required this.initials, this.index})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    if (initials.length < 3) {
+      index = initials.length;
+    } else {
+      index = 3;
+    }
+
+    return Container(
+      height: size.toFont,
+      width: size.toFont,
+      decoration: BoxDecoration(
+        color: ContactInitialsColors.getColor(initials),
+        borderRadius: BorderRadius.circular(size.toWidth),
+      ),
+      child: Center(
+        child: Text(
+          initials.substring((index == 1) ? 0 : 1, index).toUpperCase(),
+          style: CustomTextStyles.whiteBold(size: (size ~/ 3)),
+        ),
+      ),
+    );
+  }
+}
