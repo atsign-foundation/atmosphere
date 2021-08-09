756a58810bc72ed01dfa66cea2ef060ae5461898
sarika gautam <sarikagautam@Sarika-GautamB114s-MacBook-Pro.local>
2021-08-09 17:21:51 +0530

    added back constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,25 @@
+class MixedConstants {
+  // static const String WEBSITE_URL = 'https://staging.atsign.wtf/';
+  static const String WEBSITE_URL = 'https://atsign.com/';
+
+  // for local server
+  // static const String ROOT_DOMAIN = 'vip.ve.atsign.zone';
+  // for staging server
+  // static const String ROOT_DOMAIN = 'root.atsign.wtf';
+  // for production server
+  static const String ROOT_DOMAIN = 'root.atsign.org';
+
+  static const String TERMS_CONDITIONS = 'https://atsign.com/terms-conditions/';
+  // static const String PRIVACY_POLICY = 'https://atsign.com/privacy-policy/';
+  static const String PRIVACY_POLICY =
+      "https://atsign.com/apps/atmosphere/atmosphere-privacy/";
+
+  // the time to await for file transfer acknowledgement in milliseconds
+  static const int TIME_OUT = 60000;
+  static String appNamespace = 'mosphere';
+  static String regex =
+      '(.$appNamespace|atconnections|[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12})';
+
+  // Onboarding API key - requires different key for production
+  static String ONBOARD_API_KEY = '477b-876u-bcez-c42z-6a3d';
+}
